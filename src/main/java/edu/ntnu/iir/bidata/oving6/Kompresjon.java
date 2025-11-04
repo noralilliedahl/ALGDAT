4b2c7d425eff7883b7dd0ee380902881ce580b05
Nora Lilliedahl Kirknes <noralilliedahl@gmail.com>
2025-11-04 14:36:43 +0100

    feat: hoffman-class with node-class representing the tree-nodes.

--- src/main/java/edu/ntnu/iir/bidata/oving6/Kompresjon.java
+++ src/main/java/edu/ntnu/iir/bidata/oving6/Kompresjon.java
@@ -1,6 +1,7 @@
 package edu.ntnu.iir.bidata.oving6;
 
 import java.io.*;
+import java.util.Arrays;
 
 
 class BitWriter {
@@ -30,23 +31,23 @@ class BitWriter {
   void writeBits(long value, int length) throws IOException {
     if (length < 0) throw new IllegalArgumentException("length < 0");
     for (int i = length - 1; i >= 0; i--) {
-      int bit = (int) ((value >> i) & 1);
+      int bit = (int) ((value >> i) & 1L);
       writeBit(bit);
     }
   }
 
-  void flush() throws IOException {
+  public void flush() throws IOException {
     if (usedBits > 0) {
       buffer = buffer << (8 - usedBits) & 0xFF;
       out.write(buffer);
       buffer = 0;
       usedBits = 0;
-      out.flush();
     }
+    out.flush();
   }
 
   void close() throws IOException {
-    out.flush();
+    flush();
     out.close();
   }
 }
@@ -82,13 +83,81 @@ class BitReader {
     if (length < 0) throw new IllegalArgumentException("length < 0");
     long acc = 0;
     for (int i = 0; i < length; i++) {
-      acc <<= 1 | readBit();
+      acc = (acc << 1) | readBit();
     }
     return acc;
   }
 
   void close() throws IOException {
     in.close();
+  }
+}
+
+class FrequencyTable {
+
+  private final int[] counts = new int[256]; //2^8
+
+  public FrequencyTable() {
+
+  }
+
+  void reset() {
+    Arrays.fill(counts, 0);
+  }
+
+  void count(InputStream in) throws IOException {
+    int b;
+    while ((b = in.read()) != -1) { counts[b & 0xFF]++; }
+  }
+
+  long total() {
+    long sum = 0;
+    for (int i = 0; i < 256; i++) {
+      sum += counts[i];
+    }
+    return sum;
+  }
+
+  void writeTable(OutputStream out) throws IOException {
+    DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(out));
+    for (int i = 0; i < 256; i++) {
+      dout.writeInt(counts[i]);
+    }
+    dout.flush();
+  }
+
+  void readTable(InputStream in) throws IOException {
+    DataInputStream din = new DataInputStream(new BufferedInputStream(in));
+    for (int i = 0; i < 256; i++) {
+      counts[i] = din.readInt();
+    }
+  }
+}
+
+class Huffman {
+
+  class Node {
+
+    long frequency;
+    int symbol;
+    Node left, right;
+
+    public Node(long frequency, int symbol) {
+      this.frequency = frequency;
+      this.symbol = symbol;
+      this.left = null;
+      this.right = null;
+    }
+
+    public Node(Node left, Node right) {
+      this.frequency = left.frequency + right.frequency;
+      this.symbol = -1;
+      this.left = left;
+      this.right = right;
+    }
+
+    boolean isLeaf() { return left == null && right == null; }
+
   }
 }
 
